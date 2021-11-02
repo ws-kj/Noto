3590b76ed4b21da0ff95d568fd19fcd5b20e5ad8
Will Savage <wsavage6316@gmail.com>
2021-11-01 20:50:54 -0400

    added tui printing/prompt

--- test/main.go
+++ test/main.go
@@ -6,13 +6,17 @@ import (
 )
 
 var (
-    tui *gocui.Gui
+    tui    *gocui.Gui
     shell  *gocui.View
     oldX   int
     oldY   int
+    boundX     int
+    boundY     int
+    first  bool //dumb hack
 )
 
-func InitGui() {
+func InitTui() {
+    first = true
     g, err := gocui.NewGui(gocui.OutputNormal)
     if err != nil {
         log.Fatal(err)
@@ -27,6 +31,7 @@ func InitGui() {
     if err := tui.MainLoop(); err != nil && err != gocui.ErrQuit {
         log.Fatal(err)
     }
+//    shell.MoveCursor(0, 1, false)
 }
 
 func shell_editor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
@@ -36,18 +41,29 @@ func shell_editor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
         case key == gocui.KeySpace:
             v.EditWrite(' ')
         case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
-            v.EditDelete(true)
+            cx, cy := shell.Cursor()
+            if !(cy == boundY && cx <= boundX) {
+                v.EditDelete(true)
+            }
         case key == gocui.KeyArrowLeft:
-            v.MoveCursor(-1, 0, false)
+            cx, cy := shell.Cursor()
+            if !(cy == boundY && cx <= boundX) {
+                v.MoveCursor(-1, 0, false)
+            }
         case key == gocui.KeyArrowRight:
             v.MoveCursor(1, 0, false)
+            
+        case key == gocui.KeyEnter:
+            prompt()
     }
 }
 
-func set_text(v *gocui.View, text string) {
-    v.Clear()
+func prompt() {
+    Shprint("\nNoto ?> ")
+}
+
+func VPrint(v *gocui.View, text string) {
     v.Write([]byte(text))
-    v.SetCursor(len(text), 0)
 }
 
 func shell_manager(g *gocui.Gui) error {
@@ -71,19 +87,50 @@ func shell_manager(g *gocui.Gui) error {
     }
     if oldX != maxX {
         ox, oy := shell.Cursor()
-        l := (oy * oldX) + ox
-        nx := l % maxX
-        if oldX < maxX { nx -= 2 }  //why??
-        if oldX > maxX { nx += 2 }  //why does this work? i do not know
-        shell.SetCursor(nx, ((l - (l % maxX)) / maxX)) 
+        shell.SetCursor(resize_pos(ox, oy, oldX, oldY, maxX, maxY))
     }
     oldX = maxX
     oldY = maxY
+
+    if first {
+        first = false
+        Shprintln("Noto v0.1\n")
+        prompt()
+    }
+
     return nil
 }
 
-func pshell(t string) {
-    set_text(shell, t)
+func resize_pos(ox int, oy int, omx int, omy int, nmx int, nmy int) (int, int) {
+    l := (oy * omx) + ox
+    nx := l % nmx
+    if omx < nmx { nx -= 2 } //no idea why this works
+    if omx > nmx { nx += 2 } //^                    ^
+    return nx, (l - (l % nmx)) / nmx
+}
+
+func Shprintln(t string) {
+    _, cy := shell.Cursor()
+    r := []rune(t)
+    for i := 0; i < len(r); i++ {
+        if r[i] == '\n' { cy++; }
+    }
+    VPrint(shell, t+"\n")
+    shell.SetCursor(0, cy+1)
+    boundX, boundY = shell.Cursor()
+}
+
+func Shprint(t string) {
+    cx, cy := shell.Cursor()
+    
+    r := []rune(t)
+    for i := 0; i < len(r); i++ {
+        if r[i] == '\n' { cy++; cx = 0 }
+    }
+   
+    shell.SetCursor(cx + len(r)-1, cy)
+    VPrint(shell, t)
+    boundX, boundY = shell.Cursor()
 }
 
 func quit(_ *gocui.Gui, _ *gocui.View) error {
@@ -91,5 +138,5 @@ func quit(_ *gocui.Gui, _ *gocui.View) error {
 }
 
 func main() {
-    InitGui()
+    InitTui()
 }
